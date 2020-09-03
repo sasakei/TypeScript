9aa2769dd02b24baaf8623b797f3068de75d2886
sasaki kei <sasakei1021@icloud.com>
2020-09-03 09:58:41 +0900

    class

---
+++ src/my-first-class.ts
@@ -0,0 +1,18 @@
+export {};
+
+class Person {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  profile(): string {
+    return `name: ${this.name}, age: ${this.age}`;
+  }
+}
+
+let Kei = new Person('Kei', 23);
+console.log(Kei.profile());
