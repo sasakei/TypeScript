ad234c8a4e665b0d1363a6e394beeea6ceec9491
sasaki kei <sasakei1021@icloud.com>
2020-09-09 20:56:08 +0900

    getter-setter

--- src/getter-setter.ts
+++ src/getter-setter.ts
@@ -14,10 +14,18 @@ class MyNumberCard {
   }
 
   set secretNumber(secretNumber: number) {
-    this.secretNumber = secretNumber;
+    this._secretNumber = secretNumber;
+  }
+
+  debugPrint() {
+    return `${this._secretNumber}`;
   }
 }
 let card = new MyNumberCard('KEI', 12345);
 console.log(card.owner);
+console.log(card.debugPrint());
+card.secretNumber = 12346;
+console.log(card.debugPrint());
+
 // card._owner = 'SASAKI';
 // console.log(card._secretNumber);
