be60eee0316eb415047103aee7c16952723fb27d
sasaki kei <sasakei1021@icloud.com>
2020-09-17 22:01:25 +0900

    assertion

---
+++ src/type-assertion.ts
@@ -0,0 +1,11 @@
+export {};
+
+let name: any = 'Kei';
+
+// let length = (name as string).length;
+let length = (<string>name).length;
+
+// length = 'Sasa';
+// ⬆︎エラーが起きるので正しくアノテーションされている
+
+console.log();
